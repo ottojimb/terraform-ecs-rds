2977506fbe0dd60b13950180b06eb541d7afccde
Otoniel Jiménez <ottojimenez@gmail.com>
2021-12-13 16:50:23 -0500

    First commit

---
+++ main.tf
@@ -0,0 +1,197 @@
+resource "aws_s3_bucket" "automa" {
+  bucket = "automa-automa"
+  versioning {
+    enabled = true
+  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+  tags = {
+    Name = "terraform remote state"
+  }
+}
+
+terraform {
+  # backend "s3" {
+  #   bucket = "automa-automa"
+  #   key    = "terraform/automa/key"
+  #   region = "us-east-1"
+  #   encrypt = true
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "automa" {
+  name                 = "${var.project}-${terraform.workspace}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = terraform.workspace
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "${var.project}-${terraform.workspace}"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "${var.project}-${terraform.workspace}",
+      "image": "${aws_ecr_repository.automa.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8000,
+          "hostPort": 8000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.my_task.family
+    container_port   = 8000
+  }
+
+  network_configuration {
+    subnets = [
+      "${aws_default_subnet.default_subnet_a.id}",
+      "${aws_default_subnet.default_subnet_b.id}",
+      "${aws_default_subnet.default_subnet_c.id}"
+    ]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf"
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}",
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our tagrte group
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
